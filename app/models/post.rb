cbfa00f444a5bc7534ccea0e12c5db96c0c30347
Alex Merecka <merecka@gmail.com>
2019-08-13 00:46:59 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+
+	validates :title, presence: true
+	validates :title, inclusion: {in: %w(Won't Believe, Secret, Top[number], Guess)}
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
 end
