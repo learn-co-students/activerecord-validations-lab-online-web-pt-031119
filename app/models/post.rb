7bcacbfe671c753ab0bd3145621842c2b33199fa
Alex Merecka <merecka@gmail.com>
2019-08-13 16:32:01 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
 
 	validates :title, presence: true
-	validates :title, inclusion: {in: %w(Won't Believe, Secret, Top[number], Guess)}
+	# validates :title, inclusion: {in: %w("Won't Believe"+, "Secret"+, "Top[number]"+, "Guess"+)}
+	# validates :title, inclusion: {in: ["Won't Believe", "Secret", "Top[number]", "Guess"]}
 	validates :content, length: {minimum: 250}
 	validates :summary, length: {maximum: 250}
 	validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
+
 end
