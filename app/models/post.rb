977e549877e15da3c2721fad2c9511b24c4471f1
Alex Merecka <merecka@gmail.com>
2019-08-13 19:01:23 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ class Post < ActiveRecord::Base
 	# validates :title, inclusion: {in: ["Won't Believe", "Secret", "Top[number]", "Guess"]}
 	validates :content, length: {minimum: 250}
 	validates :summary, length: {maximum: 250}
-	validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
+	validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+	validate :title_contains_this
+
+	def title_contains_this
+		["Won't Believe", "Secret", "Top[number]", "Guess"].any? { |s| self.title.include? s}
+	end
 
 end
